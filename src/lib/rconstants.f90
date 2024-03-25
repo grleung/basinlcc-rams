8e9d6d96988cc9bf977c4a55aa73dc8e01cd8ba2
grleung <gabrielle.leung@colostate.edu>
2024-03-25 10:32:35 -0700

    set some constants to be same as RCEMIP

--- src/lib/rconstants.f90
+++ src/lib/rconstants.f90
@@ -3,23 +3,25 @@ Module rconstants
 
 implicit none
 
+! GRL 2024-03-25 changed some constants to be consistent with RCEMIP
+
 real, parameter ::                    &
-        rgas     = 287.               & !Dry gas constant (J/kg/K)
-    ,   cp       = 1004.              & !Specific heat at const pressure (J/kg/K)
+        rgas     = 287.04             & !Dry gas constant (J/kg/K)
+    ,   cp       = 1004.64            & !Specific heat at const pressure (J/kg/K)
     ,   cv       = 717.               & !Specific heat at const volumne (J/kg/K)
-    ,   rm       = 461.               &
+    ,   rm       = 461.50             &
     ,   p00      = 1.e5               & !Standard pressure (Pa)
     ,   t00      = 273.16             & !Freezing point (K)
-    ,   g        = 9.80               & !Gravity (m/s^2)
+    ,   g        = 9.79765            & !9.80               & !Gravity (m/s^2)
     ,   pi180    = 3.1415927 / 180.   &
     ,   pi4      = 3.1415927 * 4.     &
     ,   spcon    = 111120.            &
-    ,   erad     = 6367000.           & !Earth radius (m)
+    ,   erad     = 6371000.           & !6367000.           & !Earth radius (m)
     ,   vonk     = 0.40               & !von Karman constant
     ,   tkmin    = 5.e-4              &
-    ,   alvl     = 2.50e6             & !Latent heat of vaporization at 0C (J/kg)
+    ,   alvl     = 2.501e6             & !Latent heat of vaporization at 0C (J/kg)
     ,   alvi     = 2.834e6            & !Latent heat of sublimation at 0C (J/kg)
-    ,   alli     = 0.334e6            & !Latent heat of fusion at 0C (J/kg)
+    ,   alli     = 0.3337e6            & !Latent heat of fusion at 0C (J/kg)
     ,   alvl2    = 6.25e12            &
     ,   alvi2    = 8.032e12           &
     ,   solar    = 1.3533e3           &
